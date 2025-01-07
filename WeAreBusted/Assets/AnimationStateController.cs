bc57d41663c12bab760daeab48f81b857cdd8722
xMaarass <marasekdamian@gmail.com>
2025-01-07 21:19:53 +0100

    Character animation v1
    Dodana animacja stania, chodzenia oraz biegania

--- WeAreBusted/Assets/AnimationStateController.cs
+++ WeAreBusted/Assets/AnimationStateController.cs
@@ -3,31 +3,44 @@ using UnityEngine;
 public class AnimationStateController : MonoBehaviour
 {
     Animator animator;
-    int isWalkingHash;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = GetComponent<Animator>();
-        isWalkingHash = Animator.StringToHash("isWalking");
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool isWalking = animator.GetBool("isWalkingHash");
+        bool isWalking = animator.GetBool("isWalking");
+        bool isRunning = animator.GetBool("isRunning");
         bool forwardPressed = Input.GetKey(KeyCode.W);
+        bool backPressed = Input.GetKey(KeyCode.S);
+        bool leftPressed = Input.GetKey(KeyCode.A);
+        bool rightPressed = Input.GetKey(KeyCode.D);
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
 
-
-        if (!isWalking && forwardPressed)
+        //Idle animation to walk animation
+        if (!isWalking && (forwardPressed || backPressed || leftPressed || rightPressed))
         {
-            animator.SetBool("isWalkingHash", true);
+            animator.SetBool("isWalking", true);
         }
 
-        if (isWalking && !forwardPressed)
+        //Walk animation to idle animation
+        if (isWalking && !forwardPressed && !backPressed && !leftPressed && !rightPressed)
         {
-            animator.SetBool("isWalkingHash", false);
+            animator.SetBool("isWalking", false);
         }
 
+        if (isWalking && runPressed)
+        {
+            animator.SetBool("isRunning", true);
+        }
+
+        if (isRunning && !runPressed)
+        {
+            animator.SetBool("isRunning", false);
+        }
     }
 }
